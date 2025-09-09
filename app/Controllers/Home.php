fe7e13f265318b34edb981327aef2031f23a7820
Your New Name <your-email@example.com>
2025-09-09 10:57:40 +0800

    Added Bootstrap template and Home controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $data = [
+            'title' => 'Home Page'
+        ];
+
+        // Load the custom Bootstrap homepage
+        return view('home', $data);
     }
 }
---
+++ app/Views/home.php
@@ -0,0 +1,13 @@
+<?= $this->extend('template') ?>
+
+<?= $this->section('content') ?>
+<div class="container mt-5">
+    <div class="text-center">
+        <h1 class="display-4">Welcome to My CodeIgniter Project</h1>
+        <p class="lead">This is your new Bootstrap-powered homepage.</p>
+        <hr class="my-4">
+        <p>Start building your web application with CodeIgniter and Bootstrap.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Get Started</a>
+    </div>
+</div>
+<?= $this->endSection() ?>
